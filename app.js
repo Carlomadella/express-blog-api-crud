34798e3c42b01cc364fd42823567cfd509369db4
Carlomadella <madella871@gmail.com>
2025-04-28 18:57:13 +0200

    [FEAT] importato e utilizzato i middleware utili nella mia pagina principale app.js

--- app.js
+++ app.js
@@ -6,11 +6,12 @@ const app = express()
 // inizializzo il numero di porta in cui voglio che il server ascolti
 const port = 3000
 
+// importo il router dei post
+const postsRouter = require('./routers/posts.js');
 
-
-// IMPORTARE IL MIDDLEWARE
-
-
+// importo i middleware
+const notFound = require('./middlewares/notFound.js')
+const errorsHandler = require('./middlewares/errorsHandler.js')
 
 // utilizzo il router importato precedentemente
 app.use('/posts', postsRouter)
@@ -20,11 +21,15 @@ app.get('/', (req, res) => {
     res.send('Homepage')
 });
 
+// utilizzo i middleware
+app.use(errorsHandler);
+app.use(notFound);
+
+
 // indichiamo al server di rimanere in ascolto sulla porta 3000 impostata precedentemente
 app.listen(port, () => {
     console.log(`Server in ascolto alla porta ${3000}`)
 });
 
-// importo il router dei post
-const postsRouter = require('./routers/posts.js');
+
 
